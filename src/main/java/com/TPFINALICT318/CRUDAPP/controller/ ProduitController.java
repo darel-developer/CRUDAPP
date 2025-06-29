3826f88b47c2f56660ac1abe9882ebf5168967ab
YvanNjanko <yvannjanko04@gmail.com>
2025-06-29 09:52:28 +0100

    add product controller

---
+++ src/main/java/com/TPFINALICT318/CRUDAPP/controller/ ProduitController.java
@@ -0,0 +1,66 @@
+package com.ict.tp_exam.controller;
+
+import com.ict.tp_exam.model.Produit;
+import com.ict.tp_exam.service.CategoryService;
+import com.ict.tp_exam.service.ProduitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/produits")
+public class ProduitController {
+    @Autowired
+    private ProduitService produitService;
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String getAllProduits(Model model) {
+        List<Produit> produits = produitService.getAllProduits();
+        model.addAttribute("produits", produits);
+        model.addAttribute("success", model.containsAttribute("success"));
+        model.addAttribute("activePage", "produits");
+        return "produits";
+    }
+
+    @GetMapping("/new")
+    public String showProduitForm(Model model) {
+        model.addAttribute("produit", new Produit());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("activePage", "produits");
+        return "produitForm";
+    }
+
+    @PostMapping
+    public String createProduit(@ModelAttribute Produit produit) {
+        produitService.saveProduit(produit);
+        return "redirect:/produits?success=true";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable UUID id, Model model) {
+        Produit produit = produitService.getProduitById(id)
+                .orElseThrow(() -> new RuntimeException("Produit non trouvé"));
+        model.addAttribute("produit", produit);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("activePage", "produits");
+        return "produitForm";
+    }
+
+    @PostMapping("/update")
+    public String updateProduit(@ModelAttribute Produit produit) {
+        produitService.saveProduit(produit);
+        return "redirect:/produits?success=true";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduit(@PathVariable UUID id) {
+        produitService.deleteProduit(id);
+        return "redirect:/produits";
+    }
+}
