1d0490086251acb1ed606aac6a219cf7a4c98cd4
Simon Etoa👽 <123867758+SimDevFlow@users.noreply.github.com>
2025-06-30 14:47:46 +0100

    ajout du catégorie controller

---
+++ src/main/java/com/TPFINALICT318/CRUDAPP/controller/CategoryController.java
@@ -0,0 +1,61 @@
+package com.ict.tp_exam.controller;
+
+import com.ict.tp_exam.model.Category;
+import com.ict.tp_exam.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String getAllCategories(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        model.addAttribute("activePage", "categories");
+        return "categories";
+    }
+
+    @GetMapping("/new")
+    public String showCategoryForm(Model model) {
+        Category category = new Category();
+        model.addAttribute("category", category);
+        model.addAttribute("activePage", "categories");
+        return "categoryForm";
+    }
+
+    @PostMapping
+    public String createCategory(@ModelAttribute Category category) {
+        categoryService.saveCategory(category);
+        return "redirect:/categories?success=true";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable String id, Model model) {
+        Category category = categoryService.getCategoryById(id)
+                .orElseThrow(() -> new RuntimeException("Catégorie non trouvée"));
+        model.addAttribute("category", category);
+        model.addAttribute("activePage", "categories");
+        return "categoryForm";
+    }
+
+    @PostMapping("/update")
+    public String updateCategory(@ModelAttribute Category category) {
+        categoryService.saveCategory(category);
+        return "redirect:/categories?success=true";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable String id) {
+        categoryService.deleteCategory(id);
+        return "redirect:/categories";
+    }
+}
